67f1ed150820753c417742653b6a9e7e88a44ae6
Haseeb Khalid <david.brandboosters@gmail.com>
2024-01-12 19:01:04 +0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import './App.css';
+import { useState } from "react";
+import imgBulbOff from "./img/light-bulb-off.png";
+import imgBulbOn from "./img/light-bulb-on.png";
 
 function App() {
+  const [bulbOn, setBulbOn] = useState(false);
+  
   return (
-    <h1>State Management</h1>
+    <>
+      <h1>Bulbs</h1>
+      <div>
+        <img src={bulbOn ? imgBulbOff : imgBulbOn} alt="bulb" width={500}/>
+        <button onClick={() => setBulbOn(!bulbOn)}>{bulbOn ? 'Turn Off' : 'Turn On'}</button>
+      </div>
+    </>
   );
 }
 
